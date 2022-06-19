286e97f45fa77c45d05d92b78140dce1494f9023
Michael Tillman <mnamllit@gmail.com>
2022-06-19 03:30:59 -0500

    Added description and ID to constructor instance attributes. Also added a get name method and a __repr__ to the Product class.

--- main.py
+++ main.py
@@ -5,4 +5,11 @@ stock = []
 
 
 test1 = Product('sample')
+test2 = Product('sample2')
 stock.append(test1)
+stock.append(test2)
+
+
+
+print(test1.id)
+print(test2.id)
--- product.py
+++ product.py
@@ -1,5 +1,15 @@
 class Product:
 
+    id = 1
+
     def __init__(self, name):
         self.name = name
+        self.descripion = ''
+        self.id = Product.id
+        Product.id += 1
+
+    def name(self):
+        return self.name
     
+    def __repr__(self):
+        return self.name
