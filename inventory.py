4f38756616efaba21ba24f4937fcd7f07cb10645
Michael Tillman <mnamllit@gmail.com>
2022-06-19 04:46:08 -0500

    Added a function to show the list of available stock.

--- inventory.py
+++ inventory.py
@@ -1,19 +1,45 @@
+from product import Product
+
+from prettytable import PrettyTable
+
 class Inventory():
 
+    stock = []
+
+    for i in range(10):
+        sample = Product(f'Test{i}')
+        stock.append(sample)
 
     def __init__(self):
         self.menu()
 
-    
     def menu(self):
+        # Options menu
+
         print('Greetings, Human!\n')
         print('What would you like to do from the available options?\n')
 
         options = input(f'1) Show Available Inventory\n2) Add To Inventory\n3) Remove from inventory\n4) Exit Program\n\n>>> ')
-
+        if options == '1':
+            self.show_items()
         if options == '4':
-            print()
-            print('Now exiting program')
+            print('\nNow exiting program')
             return
+    
+    def show_items(self):
+        # Display items in stock
+
+        my_table = PrettyTable()
+        my_table.field_names = ['ID', 'Name']
+
+        for item in Inventory.stock:
+            my_table.add_row((item.id, item.name))
+        print(my_table)
+
+        confirm = input('Would you like to go back to the menu?\n(Enter yes to go back to the menu.)\n')
+
+        if confirm == 'yes' or confirm == 'y':
+            self.menu()
+
     
 
--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 from product import Product
 from inventory import Inventory
 
-stock = []
-
-for i in range(10):
-    sample = Product(f'Test{i}')
-    stock.append(sample)
-
-
-
 main_program = Inventory()
